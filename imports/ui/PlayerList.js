5215e1e904f35abba44dcc0209731b7c349e7a5a
cannell001 <cannell001@gannon.edu>
2018-11-08 13:25:51 -0500

    Conditional Render with JSX

--- imports/ui/PlayerList.js
+++ imports/ui/PlayerList.js
@@ -4,11 +4,15 @@ import PropTypes from 'prop-types';
 import Player from './Player';
 
 export default class PlayerList extends Component {
-  renderPlayers() {
-    return this.props.players.map((player) => {
-      return <Player key={player._id} player={player}/>;
-    });
-  }
+    renderPlayers() {
+      if (this.props.players.length === 0) {
+        return <p>Add your first player to get started!</p>;
+      } else {
+        return this.props.players.map((player) => {
+          return <Player key={player._id} player={player}/>;
+        });
+      }
+    }
   render() {
     return (
       <div>
